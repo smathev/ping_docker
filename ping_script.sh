2c82620c3751ff4d130664b005a2d52b37ab3faa
core <core@ubuntu.home>
2019-03-26 09:12:47 +0100

    new

--- ping_script.sh
+++ ping_script.sh
@@ -2,13 +2,16 @@
 
 host=http://www.google.com
 
-log_file=/config/log
-reaction_file=/config/working
+log_file=log
+reaction_file=working
+ip_file=ip
 
 wget -q --tries=10 --timeout=20 --spider $host
 if [ $? -eq 0 ]; 
     then
         echo "connection UP - $(date)" > $log_file
+        wget -qqO- 'https://duckduckgo.com/?q=what+is+my+ip' \
+        | grep -Pow 'Your IP address is \K[0-9.]+' > $ip_file
         touch $reaction_file
 else
         echo "connection DOWN - $(date)" > $log_file
