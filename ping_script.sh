795b7a4b5b0d6c976a73fd074553b053b59bda08
core <core@ubuntu.home>
2019-04-10 14:12:49 +0200

    added VPN-conf changer

--- ping_script.sh
+++ ping_script.sh
@@ -6,14 +6,22 @@ log_file=/config/log
 reaction_file=/config/working
 ip_file=/config/ip
 
+pingCheckCommand () {
+wget -qqO- 'https://duckduckgo.com/?q=what+is+my+ip' | grep -Pow 'Your IP address is \K[0-9.]+' > $ip_file
+}
+
+switchVpnConf () {
+find /home/core/.docker/configs/mullvad_vpn-client/mullvad_config_files/ -type f | shuf -n 1 | xargs cp  -t /home/core/.docker/configs/mullvad_vpn-client/act.Cert/vpn.conf
+}
+
 wget -q --tries=10 --timeout=20 --spider $host
 if [ $? -eq 0 ]; 
     then
         echo "connection UP - $(date)" > $log_file
-        wget -qqO- 'https://duckduckgo.com/?q=what+is+my+ip' \
-        | grep -Pow 'Your IP address is \K[0-9.]+' > $ip_file
+        pingCheckCommand
         touch $reaction_file
 else
         echo "connection DOWN - $(date)" > $log_file
+        switchVpnConf
         rm -rf $reaction_file
 fi
