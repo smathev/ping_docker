0c57e78fdb2bfa6562531d9440c81c34e138a223
core <core@ubuntu.home>
2019-05-05 21:52:49 +0200

    Corrected first run of speedtest-cli in entry.sh

--- entry.sh
+++ entry.sh
@@ -5,7 +5,7 @@ sleep 20
 /bin/sh /config/ping_script.sh
 
 # run a first speedtest and clean output
-/usr/bin/speedtest-cli > /config/speedtest.csv
+/usr/bin/speedtest-cli --csv > /config/speedtest.csv
 /bin/sh /config/speedtest_output.sh
 
 # start cron
